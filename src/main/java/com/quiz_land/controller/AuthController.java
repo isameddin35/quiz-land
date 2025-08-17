66456d87657ae61c2d969d0d559c1b92e2abc8c8
Isameddin <isaqurbanov112@gmail.com>
2025-08-17 14:18:22 +0400

    Home page text added

--- src/main/java/com/quiz_land/controller/AuthController.java
+++ src/main/java/com/quiz_land/controller/AuthController.java
@@ -5,10 +5,7 @@ import com.quiz_land.dto.RegisterDto;
 import com.quiz_land.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/auth")
@@ -16,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final AuthService authService;
+
+
+    @GetMapping
+    public String hello() {
+        return "Welcome to QuizLand";
+    }
 
     @PostMapping("register")
     public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
